67222ef7a9d8593aee77225a2a4f734730811efb
gabrielzv1233 <77300296+gabrielzv1233@users.noreply.github.com>
2023-09-11 14:41:26 +0000

    ok I got it to work I think

---
+++ filters.py
@@ -0,0 +1,7 @@
+block = {
+    "fuck": "f**k",
+    "nigg": "n**g",
+    "nigga": "n***a",
+    "nigger": "n***er",
+    "cunt": "c**t"
+}
--- main.py
+++ main.py
@@ -4,104 +4,111 @@ import datetime
 import os
 from html import escape
 import json
+from filters import block
 
 app = Flask(__name__)
 
-
-#Route for the 404 error page
+# Route for the 404 error page
 @app.errorhandler(404)
 def page_not_found(e):
-  return render_template('404.html'), 404
-
+    return render_template('404.html'), 404
 
 @app.route('/')
 def serve_html():
-  return render_template('index.html')
-
+    return render_template('index.html')
 
 @app.route('/send', methods=['GET', 'POST'])
 def receive_message():
-  if request.method == 'POST':
-    user = escape(request.form.get('user'))
-    message = escape(request.form.get('msg'))
+    if request.method == 'POST':
+        user = escape(request.form.get('user'))
+        message = escape(request.form.get('msg'))
 
-    if not user or not message:
-      return redirect(url_for('send'))
+        if not user or not message:
+            return redirect(url_for('send'))
 
-    server_time = datetime.datetime.now().strftime("%H:%M")
-    url = f"https://msgr.gabrielzv1233.repl.co/send?user={user}&msg={message}&time={server_time}"
+        # Apply chat filtering to username and message
+        for word in block:
+            if word.lower() in user.lower():
+                user = user.replace(word, block[word])
+            if word.lower() in message.lower():
+                message = message.replace(word, block[word])
 
-    response = requests.get(url)
+        server_time = datetime.datetime.now().strftime("%H:%M")
+        url = f"https://msgr.gabrielzv1233.repl.co/send?user={user}&msg={message}&time={server_time}"
 
-    if response.status_code == 200:
-      with open('messages.txt', 'a') as file:
-        file.write(
-            f'<b>{user}</b> <i>@<u>{server_time}</u></i>: {message}<br>\n')
-      return render_template('message_sent.html')
-    else:
-      return 'Failed to send the message.'
+        response = requests.get(url)
 
-  return redirect(url_for('send'))
+        if response.status_code == 200:
+            with open('messages.txt', 'a') as file:
+                file.write(f'<b>{user}</b> <i>@<u>{server_time}</u></i>: {message}<br>\n')
+            return render_template('message_sent.html')
+        else:
+            return 'Failed to send the message.'
 
+    return redirect(url_for('send'))
 
 @app.route('/api/send', methods=['GET'])
 def send_message_query():
-  user = request.args.get('user')
-  message = request.args.get('msg')
+    user = request.args.get('user')
+    message = request.args.get('msg')
 
-  if not user or not message:
-    return 'Invalid request.<br> make sure both username and message fields have a non-space character'
+    if not user or not message:
+        return 'Invalid request.<br> make sure both username and message fields have a non-space character'
 
-  user = escape(user)
-  message = escape(message)
+    user = escape(user)
+    message = escape(message)
 
-  server_time = datetime.datetime.now().strftime("%H:%M")
+    # Apply chat filtering to username and message
+    for word in block:
+        if word.lower() in user.lower():
+            user = user.replace(word, block[word])
+        if word.lower() in message.lower():
+            message = message.replace(word, block[word])
 
-  with open('messages.txt', 'a') as file:
-    file.write(f'<b>{user}</b> <i>@<u>{server_time}</u></i>: {message}<br>\n')
+    server_time = datetime.datetime.now().strftime("%H:%M")
 
-  return 'Message sent successfully.'
+    with open('messages.txt', 'a') as file:
+        file.write(f'<b>{user}</b> <i>@<u>{server_time}</u></i>: {message}<br>\n')
 
+    return 'Message sent successfully.'
 
 @app.route('/full', methods=['GET'])
 def display_messages():
-  if not os.path.exists('messages.txt'):
-    with open('messages.txt', 'w') as file:
-      pass
-  elif os.stat('messages.txt').st_size == 0:
-    pass
+    if not os.path.exists('messages.txt'):
+        with open('messages.txt', 'w') as file:
+            pass
+    elif os.stat('messages.txt').st_size == 0:
+        pass
 
-  with open('messages.txt', 'r') as file:
-    messages = file.read()
+    with open('messages.txt', 'r') as file:
+        messages = file.read()
 
-  return render_template('full.html', messages=messages)
-
+    return render_template('full.html', messages=messages)
 
 @app.route('/send.html', methods=['GET'])
 def send():
-  return render_template('send.html')
+    return render_template('send.html')
 
-
-@app.route('/info.html')
+@app.route('/info')
 def info():
-  return render_template('info.html')
-
+    return render_template('info.html')
 
 @app.route('/api/get')
 def get_messages():
-  return send_file('messages.txt', mimetype='text/html')
-
+    return send_file('messages.txt', mimetype='text/html')
 
 @app.route('/api/get/raw')
 def get_raw_messages():
-  return send_file('messages.txt', mimetype='text/plain')
-
+    return send_file('messages.txt', mimetype='text/plain')
 
 @app.route('/favicon.png')
 def serve_image():
     faviconimg = 'templates/favicon.png'
     return send_file(faviconimg, mimetype='image/png')
 
+@app.route('/chatfilter')
+def serve_filter():
+    return send_file('filters.py', mimetype='text/plain')
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
