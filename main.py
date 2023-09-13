db215687c3fa92eb762547a946a84e1d2addad62
gabrielzv1233 <77300296+gabrielzv1233@users.noreply.github.com>
2023-09-13 00:50:55 +0000

    temp

--- main.py
+++ main.py
@@ -183,4 +183,4 @@ def serve_filter():
     return send_file('settings.py', mimetype='text/plain')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    #app.run(host='0.0.0.0', port=5000)
