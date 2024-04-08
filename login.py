22dd52f6a91ecef182183e0d67e5f3b82dcf894a
gabrielzv1233 <26gvurciaga@crschools.us>
2024-04-08 00:05:42 -0500

    basics

--- login.py
+++ login.py
@@ -1,82 +1,29 @@
 import shelve
-from flask import Flask, make_response, request, render_template, redirect, url_for
+from flask import Flask, make_response, request
 import uuid
 import datetime
-import logging
 app = Flask(__name__)
-
-admin_key = "3gr978"
-
-banned_uuids = {
-    "UUID": "none"
-}
-
-banned_ips = {
-    "ip address": "none"
-}
-
-log = logging.getLogger('werkzeug')
-log.setLevel(logging.ERROR)
 
 @app.route("/")
 def main():
-    print('Connection: "/" code: 200')
-    db = shelve.open('users/userdata')
+    db = shelve.open('databases/users/userdata')
     username = request.cookies.get('un')
     login_token = request.cookies.get('LOGIN_TOKEN')
     if not username or not login_token:
-        response = make_response(f'<a href="{url_for("signup")}">signup</a> or <a href="{url_for("login")}">login</a>')
-        response.delete_cookie("LOGIN_TOKEN")
-        response.delete_cookie("un")
-        return response
+        return "not logged in"
     else: 
         if username in db:
             data = db[username]
             if login_token == data[1]:
                 db.close()
-                return render_template('index.html', username=username)
+                return f'logged in as {username}<form method="POST" action="/logout"><input type="submit" value="Logout"></form><br><br><form method="POST" action="/delete_account"><input type="checkbox" required>Check this and click button below to delete account<br><input type="submit" value="Delete account">'
             else:
                 db.close()
-                response = make_response(f'<a href="{url_for("signup")}">signup</a> or <a href="{url_for("login")}">login</a>')
-                response.delete_cookie("LOGIN_TOKEN")
-                response.delete_cookie("un")
-                return response
+                return "not logged in"
             
-@app.route('/send', methods=["POST"])
-def send():
-    db = shelve.open('users/userdata')
-    username = request.cookies.get('un')
-    login_token = request.cookies.get('LOGIN_TOKEN')
-    client_ip = request.headers.get('X-Forwarded-For')
-    if client_ip in banned_ips:
-        return f"You have been banned<br>Reason:<br>{banned_ips[client_ip]}"
-    if not username or not login_token:
-        response = make_response(f'<a href="{url_for("signup")}">signup</a> or <a href="{url_for("login")}">login</a>')
-        response.delete_cookie("LOGIN_TOKEN")
-        response.delete_cookie("un")
-        return response
-    else: 
-        if username in db:
-            data = db[username]
-            if login_token == data[1]:
-                db.close()
-                if data[1] in banned_uuids:
-                    return f"You have been banned<br>Reason:<br>{banned_uuids[data[1]]}"
-                message = request.form["message"]
-                with open('static/messages.html', 'a') as file:
-                    file.write(f'{username} @{datetime.datetime.now().strftime("%H:%M")}: {message}<br>\n')
-                    print(f'Connection: "/send" user:"{username}" code: 200')
-                return redirect(url_for('main'))
-            else:
-                db.close()
-                response = make_response(f'<a href="{url_for("signup")}">signup</a> or <a href="{url_for("login")}">login</a>')
-                response.delete_cookie("LOGIN_TOKEN")
-                response.delete_cookie("un")
-                return response
-    
 @app.route("/delete_account", methods=["POST"])
 def delete_account():
-    db = shelve.open('users/userdata')
+    db = shelve.open('databases/users/userdata')
     username = request.cookies.get('un')
     login_token = request.cookies.get('LOGIN_TOKEN')
     if not username or not login_token:
@@ -91,7 +38,7 @@ def delete_account():
                 response.delete_cookie("LOGIN_TOKEN")
                 response.delete_cookie("un")
                 response.headers["Location"] = "/"
-                return response, 302
+                return response
             else:
                 db.close()
                 return "Unable to delete account: not logged in"
@@ -106,13 +53,11 @@ def logout():
 
 @app.route('/li', methods=['POST'])
 def li():
-    db = shelve.open('users/userdata')
+    db = shelve.open('databases/users/userdata')
     username = str(request.form.get('username'))
     password = str(request.form.get('password')) 
     print(username)
     print(password)
-    if client_ip in banned_ips:
-        return f"You have been banned<br>Reason:<br>{banned_ips[client_ip]}"
     if username in db:
         data = db[username]
         userpass = data[0]
@@ -132,23 +77,23 @@ def li():
 
 @app.route('/si', methods=['POST'])
 def si():
-    if client_ip in banned_ips:
-        return f"You have been banned<br>Reason:<br>{banned_ips[client_ip]}"
-    with shelve.open('users/userdata') as db:
-        username = str(request.form.get('username'))
-        data = [str(request.form.get('password')), str(uuid.uuid4())]
-        print(username)
-        print(data[0])
-        if username.lower() in [key.lower() for key in db.keys()]:
-            return "Account already exists", 200
-        else:
-            db[username] = data
-            response = make_response(f"Created account<br>{username}<br>{str(data)}")
-            expiration = datetime.datetime.now() + datetime.timedelta(days=365 * 10)
-            response.set_cookie('LOGIN_TOKEN', data[1], expires=expiration)
-            response.set_cookie('un', username, expires=expiration)
-            response.headers["Location"] = "/"
-            return response, 302
+    db = shelve.open('databases/users/userdata')
+    username = str(request.form.get('username'))
+    data = [str(request.form.get('password')), str(uuid.uuid4())]
+    print(username)
+    print(data[0])
+    if username in db:
+        db.close()
+        return "Account already exists"
+    else:
+        db[username] = data
+        response = make_response(f"Created account<br>{username}<br>{str(data)}")
+        expiration = datetime.datetime.now() + datetime.timedelta(days=365 * 10)
+        response.set_cookie('LOGIN_TOKEN', data[1], expires=expiration)
+        response.set_cookie('un', username, expires=expiration)
+        response.headers["Location"] = "/"
+        db.close()
+        return response, 302
        
 @app.route("/login")
 def login():
@@ -177,13 +122,13 @@ def signup_form():
 
 @app.route('/admin/_signup', methods=['POST'])
 def admmin_signup():
-    db = shelve.open('users/admindata')
+    db = shelve.open('databases/users/admindata')
     admin_login_key = str(request.form.get('admin_key'))
     username = str(request.form.get('username'))
     data = [str(request.form.get('password')), str(uuid.uuid4())]
     print(username)
     print(data[0])
-    if not admin_login_key == admin_key:
+    if not admin_login_key == "3gr978":
         return "Admin key incorrect"
     if username in db:
         db.close()
@@ -208,7 +153,7 @@ def admin():
 
 @app.route('/admin/login', methods=['POST'])
 def admin_login():
-    db = shelve.open('users/admindata')
+    db = shelve.open('databases/users/admindata')
     username = str(request.form.get('username'))
     password = str(request.form.get('password')) 
     print(username)
@@ -232,7 +177,7 @@ def admin_login():
     
 @app.route("/admin/panel")
 def panel():
-    db = shelve.open('users/admindata')
+    db = shelve.open('databases/users/admindata')
     username = request.cookies.get('admin_un')
     login_token = request.cookies.get('admin_LOGIN_TOKEN')
     if not username or not login_token:
@@ -241,11 +186,11 @@ def panel():
         if username in db:
             data = db[username]
             if login_token == data[1]:
-                db = shelve.open('users/userdata')
+                db = shelve.open('databases/users/userdata')
                 all_values = []
                 all_values.append("users:")
                 for key, value in db.items():
-                    all_values.append(f'{key}: {value} <form method="POST" action="/admin/delete_others"><input type="text" name="admin_key" value="{admin_key}" hidden><input name="username" type="text" value="{key}" hidden><input type="submit" value="Delete account">')
+                    all_values.append(f'{key}: {value} <form method="POST" action="/admin/delete_others"><input type="text" name="admin_key" value="3gr978" hidden><input name="username" type="text" value="{key}" hidden><input type="submit" value="Delete account">')
                 db.close()   
                 accounts = '<br>'.join(all_values)
                 return f"""logged in as {username}<form method="POST" action="/logout_admin"><input type="submit" value="Logout"></form>
@@ -265,14 +210,14 @@ def admin_logout():
 
 @app.route("/admin/delete_others", methods=["POST"])
 def admin_delete_other_account():
-    db = shelve.open('users/userdata')
+    db = shelve.open('databases/users/userdata')
     username = request.form.get('username')
     login_token = request.form.get('admin_key')
     if not username or not login_token:
         return "Unable to delete account: not logged in"
     else:
         if username in db:
-            if login_token == admin_key:
+            if login_token == "3gr978":
                 del db[username]
                 db.close()
                 response = make_response("Deleted account")
