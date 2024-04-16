0787ca7d6df6a4777c4ed994a527b07c3d7a8407
gabrielzv1233 <26gvurciaga@crschools.us>
2024-04-16 00:01:51 -0500

    new line support in messages, faster and more dynamic loading, and added metadata

--- main.py
+++ main.py
@@ -94,6 +94,9 @@ def settings():
                 return f"""<!DOCTYPE html>
             <html>
             <head>
+            <meta charset='UTF-8'>
+            <meta name='description' content='A free messaging app compleate with accounts and special syling for your message'>
+            <meta name='author' content='Gabrielzv1233'>
             <title>msgr v2</title>
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <style>
@@ -185,6 +188,9 @@ def messages():
                 return f"""<!DOCTYPE html>
             <html>
             <head>
+            <meta charset='UTF-8'>
+            <meta name='description' content='A free messaging app compleate with accounts and special syling for your message'>
+            <meta name='author' content='Gabrielzv1233'>
             <title>msgr v2</title>
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <style>
@@ -252,7 +258,7 @@ def change_messages():
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return f"""<html><title>msgr v2</title><meta name='viewport' content='width=device-width, initial-scale=1'>
+    return f"""<html><meta charset='UTF-8'><meta name='description' content='A free messaging app compleate with accounts and special syling for your message'><meta name='author' content='Gabrielzv1233'><title>msgr v2</title><meta name='viewport' content='width=device-width, initial-scale=1'>
   <head>
     <meta charset="UTF-8">
     <title>404 Page Not Found</title>
@@ -370,7 +376,7 @@ def send():
                 login_token = request.cookies.get('LOGIN_TOKEN')
                 client_ip = request.headers.get('X-Forwarded-For')
                 if client_ip in banned_ips:
-                    return f"<title>msgr v2</title><meta name='viewport' content='width=device-width, initial-scale=1'>You have been banned<br>Reason:<br>{banned_ips[client_ip]}"
+                    return f"<meta charset='UTF-8'><meta name='description' content='A free messaging app compleate with accounts and special syling for your message'><meta name='author' content='Gabrielzv1233'><title>msgr v2</title><meta name='viewport' content='width=device-width, initial-scale=1'>You have been banned<br>Reason:<br>{banned_ips[client_ip]}"
                 if not username or not login_token:
                     response = make_response(render_template('no_account.html', signup=url_for('signup'), login=url_for('login')))
                     response.delete_cookie("LOGIN_TOKEN")
@@ -382,7 +388,7 @@ def send():
                         if login_token == data[1]:
                             db.close()
                             if data[1] in banned_uuids:
-                                return f"<title>msgr v2</title><meta name='viewport' content='width=device-width, initial-scale=1'>You have been banned<br>Reason:<br>{banned_uuids[data[1]]}"
+                                return f"<meta charset='UTF-8'><meta name='description' content='A free messaging app compleate with accounts and special syling for your message'><meta name='author' content='Gabrielzv1233'><title>msgr v2</title><meta name='viewport' content='width=device-width, initial-scale=1'>You have been banned<br>Reason:<br>{banned_uuids[data[1]]}"
                             message = request.form["message"][:600]
                             with open('static/conversations/messages.html', 'a') as file:
                                 message = message.replace("<", "&#60;")
@@ -400,6 +406,9 @@ def send():
                                 message = re.sub(r"```(.*?)```", r'<div class="code">\1</div>', message)
                                 message = re.sub(r"\*\*(.*?)\*\*", r'<b>\1</b>', message)
                                 message = re.sub(r"\*(.*?)\*", r'<i>\1</i>', message)
+                                message = message.rstrip()
+                                message = message.replace("\n", "<br>")
+                                message = re.sub(r"(<br>\s*){6,}", "<br><br><br>", message)
                                 key = data[1]
                                 if data[1] in special_users:
                                     format = special_users[key]
@@ -407,7 +416,7 @@ def send():
                                 else:
                                     format = f'<b>{escape(username)}</b> <i>@{time}</i>: {message}<br>\n'
                                 if last_messages and key.lower() == last_messages[0][0].lower() and message.lower() == last_messages[0][1].lower():
-                                    response = make_response("<title>msgr v2</title><meta name='viewport' content='width=device-width, initial-scale=1'>Cannot send the same message twice")
+                                    response = make_response("<meta charset='UTF-8'><meta name='description' content='A free messaging app compleate with accounts and special syling for your message'><meta name='author' content='Gabrielzv1233'><title>msgr v2</title><meta name='viewport' content='width=device-width, initial-scale=1'>Cannot send the same message twice")
                                     response.headers["Location"] = "/"
                                     return response, 302
                                 file.write(format)
@@ -427,7 +436,7 @@ def delete_account():
     username = request.cookies.get('un')
     login_token = request.cookies.get('LOGIN_TOKEN')
     if not username or not login_token:
-        return "<title>msgr v2</title><meta name='viewport' content='width=device-width, initial-scale=1'>Unable to delete account: not logged in"
+        return "<meta charset='UTF-8'><meta name='description' content='A free messaging app compleate with accounts and special syling for your message'><meta name='author' content='Gabrielzv1233'><title>msgr v2</title><meta name='viewport' content='width=device-width, initial-scale=1'>Unable to delete account: not logged in"
     else:
         if username in db:
             data = db[username]
@@ -441,7 +450,7 @@ def delete_account():
                 return response, 302
             else:
                 db.close()
-                return "<title>msgr v2</title><meta name='viewport' content='width=device-width, initial-scale=1'>Unable to delete account: not logged in"
+                return "<meta charset='UTF-8'><meta name='description' content='A free messaging app compleate with accounts and special syling for your message'><meta name='author' content='Gabrielzv1233'><title>msgr v2</title><meta name='viewport' content='width=device-width, initial-scale=1'>Unable to delete account: not logged in"
 
 @app.route('/logout', methods=["POST"], strict_slashes=False)
 def logout():
@@ -461,7 +470,7 @@ def li():
     password = str(request.form.get('password')) 
     client_ip = request.headers.get('X-Forwarded-For')
     if client_ip in banned_ips:
-        return f"<title>msgr v2</title><meta name='viewport' content='width=device-width, initial-scale=1'>You have been banned<br>Reason:<br>{banned_ips[client_ip]}"
+        return f"<meta charset='UTF-8'><meta name='description' content='A free messaging app compleate with accounts and special syling for your message'><meta name='author' content='Gabrielzv1233'><title>msgr v2</title><meta name='viewport' content='width=device-width, initial-scale=1'>You have been banned<br>Reason:<br>{banned_ips[client_ip]}"
     if username in db:
         data = db[username]
         userpass = data[0]
@@ -486,11 +495,11 @@ def si():
     settings.close()
     client_ip = request.headers.get('X-Forwarded-For')
     if client_ip in banned_ips:
-        return f"<title>msgr v2</title><meta name='viewport' content='width=device-width, initial-scale=1'>You have been banned<br>Reason:<br>{banned_ips[client_ip]}"
+        return f"<meta charset='UTF-8'><meta name='description' content='A free messaging app compleate with accounts and special syling for your message'><meta name='author' content='Gabrielzv1233'><title>msgr v2</title><meta name='viewport' content='width=device-width, initial-scale=1'>You have been banned<br>Reason:<br>{banned_ips[client_ip]}"
     with shelve.open('data/userdata') as db:
         username = str(escape(request.form.get('username')[:22]))
         if username in filter:
-            return f"<title>msgr v2</title><meta name='viewport' content='width=device-width, initial-scale=1'>Username is not allowed"
+            return f"<meta charset='UTF-8'><meta name='description' content='A free messaging app compleate with accounts and special syling for your message'><meta name='author' content='Gabrielzv1233'><title>msgr v2</title><meta name='viewport' content='width=device-width, initial-scale=1'>Username is not allowed"
         data = [str(request.form.get('password')), str(uuid.uuid4()), client_ip]
         if username.lower() in [key.lower() for key in db.keys()]:
             return "Account already exists", 200
@@ -505,7 +514,7 @@ def si():
  
 @app.route("/login", strict_slashes=False)
 def login():
-    return """<title>msgr v2</title><meta name='viewport' content='width=device-width, initial-scale=1'><form method="POST" action="/li">
+    return """<meta charset='UTF-8'><meta name='description' content='A free messaging app compleate with accounts and special syling for your message'><meta name='author' content='Gabrielzv1233'><title>msgr v2</title><meta name='viewport' content='width=device-width, initial-scale=1'><form method="POST" action="/li">
     username: <input type="text" name="username" required maxlength="22"><br>
     password: <input type="password" name="password" required><br>
     <input type="submit" value="login">
@@ -513,7 +522,7 @@ def login():
 
 @app.route("/signup", strict_slashes=False)
 def signup():
-    return """<title>msgr v2</title><meta name='viewport' content='width=device-width, initial-scale=1'><form method="POST" action="/si" autocomplete="off">
+    return """<meta charset='UTF-8'><meta name='description' content='A free messaging app compleate with accounts and special syling for your message'><meta name='author' content='Gabrielzv1233'><title>msgr v2</title><meta name='viewport' content='width=device-width, initial-scale=1'><form method="POST" action="/si" autocomplete="off">
     username: <input type="text" name="username" required maxlength="22"><br>
     passsword: <input type="text" name="password" required><br>
     <input type="submit" value="signup">
@@ -551,7 +560,7 @@ def admmin_signup():
     
 @app.route("/admin/login", strict_slashes=False)
 def admin():
-    return """<title>msgr v2</title><meta name='viewport' content='width=device-width, initial-scale=1'><form method="POST" action="/admin/_login">
+    return """<meta charset='UTF-8'><meta name='description' content='A free messaging app compleate with accounts and special syling for your message'><meta name='author' content='Gabrielzv1233'><title>msgr v2</title><meta name='viewport' content='width=device-width, initial-scale=1'><form method="POST" action="/admin/_login">
     username: <input type="text" name="username" required><br>
     password: <input type="text" name="password" required><br>
     <input type="submit" value="login">
@@ -606,7 +615,7 @@ def panel():
                     all_values.append(f'{key} [ Password: "{value[0]}", UUID: "{value[1]}", OG-IP: "{OG_IP}" ] <form method="POST" action="/admin/delete_others"><input type="text" name="admin_key" value="{login_token}" hidden><input name="username" type="text" value="{key}" hidden><input type="submit" value="Delete account"></form>')
                 db.close()
                 accounts = '<br>'.join(all_values)
-                return f"""<html><head><title>msgr v2</title><meta name="viewport" content="width=device-width, initial-scale=1"><style>body {{background-color: #1C2333;color:white;}}input[type="submit"] {{border-radius: 5px;}}button {{border-radius: 5px;}}</style></head><body><a href="/admin/send"><button>Send from console</button></a><br><a href="/admin/messages"><button>Messages</button></a><br><a href="/admin/settings"><button>Settings</button></a><br><br>logged in as {username}<form method="POST" action="/logout_admin"><input type="submit" value="Logout"></form>
+                return f"""<html><head><meta charset='UTF-8'><meta name='description' content='A free messaging app compleate with accounts and special syling for your message'><meta name='author' content='Gabrielzv1233'><title>msgr v2</title><meta name="viewport" content="width=device-width, initial-scale=1"><style>body {{background-color: #1C2333;color:white;}}input[type="submit"] {{border-radius: 5px;}}button {{border-radius: 5px;}}</style></head><body><a href="/admin/send"><button>Send from console</button></a><br><a href="/admin/messages"><button>Messages</button></a><br><a href="/admin/settings"><button>Settings</button></a><br><br>logged in as {username}<form method="POST" action="/logout_admin"><input type="submit" value="Logout"></form>
             {accounts}
             </body></html>
             """
